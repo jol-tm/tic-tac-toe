d9ea8a4f5bf11c7fb68bc8a4de6b97eceb96fc2c
jol-tm <joaolucastmagalhaes@gmail.com>
2024-08-30 11:41:12 -0300

    Fixed first popup not displaying and increased duration of some popups

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
     $("#restartBtn").on("click", () => {
         location.reload();
     });
-    showPopUp(`Vez do "${player}"`);
+    showPopUp(`Vez do "${player}"`, 5);
 })
 
 // Matrix 3x3 to control de positions of O's and X's
@@ -72,39 +72,39 @@ function paintLine(block1, block2, block3) {
 
 function verifyGameOver() {
     if (positions[0][0] == player && positions[1][1] == player && positions[2][2] == player) {
-        showPopUp(`Vitória do ${player} na diagonal!`, 5);
+        showPopUp(`Vitória do ${player} na diagonal!`, 10);
         paintLine("00", "11", "22");
         return true;
     } else if (positions[0][2] == player && positions[1][1] == player && positions[2][0] == player) {
-        showPopUp(`Vitória do ${player} na diagonal!`, 5);
+        showPopUp(`Vitória do ${player} na diagonal!`, 10);
         paintLine("02", "11", "20");
         return true;
     } else if (positions[0][0] == player && positions[0][1] == player && positions[0][2] == player) {
-        showPopUp(`Vitória do ${player} com uma linha horizontal na 1º linha!`, 5);
+        showPopUp(`Vitória do ${player} com uma linha horizontal na 1º linha!`, 10);
         paintLine("00", "01", "02");
         return true;
     } else if (positions[1][0] == player && positions[1][1] == player && positions[1][2] == player) {
-        showPopUp(`Vitória do ${player} com uma linha horizontal na 2º linha!`, 5);
+        showPopUp(`Vitória do ${player} com uma linha horizontal na 2º linha!`, 10);
         paintLine("10", "11", "12");
         return true; 
     } else if (positions[2][0] == player && positions[2][1] == player && positions[2][2] == player) {
-        showPopUp(`Vitória do ${player} com uma linha horizontal na 3º linha!`, 5);
+        showPopUp(`Vitória do ${player} com uma linha horizontal na 3º linha!`, 10);
         paintLine("20", "21", "22");
         return true;
     } else if (positions[0][0] == player && positions[1][0] == player && positions[2][0] == player) {
-        showPopUp(`Vitória do ${player} com uma linha vertical na 1º coluna!`, 5);
+        showPopUp(`Vitória do ${player} com uma linha vertical na 1º coluna!`, 10);
         paintLine("00", "10", "20");
         return true;
     } else if (positions[0][1] == player && positions[1][1] == player && positions[2][1] == player) {
-        showPopUp(`Vitória do ${player} com uma linha vertical na 2º coluna!`, 5);
+        showPopUp(`Vitória do ${player} com uma linha vertical na 2º coluna!`, 10);
         paintLine("01", "11", "21");
         return true;
     } else if (positions[0][2] == player && positions[1][2] == player && positions[2][2] == player) {
-        showPopUp(`Vitória do ${player} com uma linha vertical na 3º coluna!`, 5);
+        showPopUp(`Vitória do ${player} com uma linha vertical na 3º coluna!`, 10);
         paintLine("02", "12", "22");
         return true; 
     } else if (turns >= 9){
-        showPopUp("Empate!", 5);
+        showPopUp("Empate!", 10);
         return true;
     } else {
         return false;
